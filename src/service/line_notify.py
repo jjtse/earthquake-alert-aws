68f825f42459c6650aa1c5ad0519748a9876bb73
jjtse <vivi999vivi999@gmail.com>
2025-01-15 16:25:47 +0800

    motify: line message api

--- src/service/line_notify.py
+++ src/service/line_notify.py
@@ -1,15 +1,28 @@
 import logging
 import requests
+import json
 
-lineUrl = "https://notify-api.line.me/api/notify"
-lineToken = "您的金鑰"
+lineUrl = "https://api.line.me/v2/bot/message/push"
+lineToken = "你的Message api金鑰"
 
 
 def lineWebhook(msg) -> None:
-    headers = {"Authorization": "Bearer " + lineToken}
-    data = {'message': msg}
+    headers = {"Authorization": "Bearer " + lineToken, 'Content-Type':'application/json'}
+    data = {
+            'to':'one-to-one user id',
+            'messages':[{
+            'type': 'text',
+            'text': "台北地區$ " + msg,
+            "emojis": [
+            {
+              "index": 0,
+              "productId": "5ac21a18040ab15980c9b43e",
+              "emojiId": "048"
+            }]
+        }]
+    }
     try:
-        response = requests.post(lineUrl, headers=headers, data=data)
+        response = requests.post(lineUrl, headers=headers, data=json.dumps(data).encode('utf-8'))
         response.raise_for_status()  # 抛出 HTTP 請求異常
         logging.info('Line Notify 發送成功')
 
